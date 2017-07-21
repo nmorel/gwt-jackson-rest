85a5750e73a8a91c97358c237edde6c846b19854
Romain Sertelon <rsertelon@users.noreply.github.com>
2017-07-21 13:13:20 +0200

    Adds support for Regex in @Path (#13)

--- processor/src/main/java/com/github/nmorel/gwtjackson/rest/processor/RestServiceMethod.java
+++ processor/src/main/java/com/github/nmorel/gwtjackson/rest/processor/RestServiceMethod.java
@@ -16,6 +16,10 @@
 
 package com.github.nmorel.gwtjackson.rest.processor;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
@@ -47,15 +51,19 @@ public class RestServiceMethod {
 
         StringBuilder urlBuilder = new StringBuilder( baseRestUrl );
         Path path = method.getAnnotation( Path.class );
+        String theUrl;
         if ( null == path ) {
-            this.url = urlBuilder.toString();
+            theUrl = urlBuilder.toString();
         } else {
             if ( !baseRestUrl.endsWith( "/" ) && !path.value().startsWith( "/" ) ) {
                 urlBuilder.append( '/' );
             }
             urlBuilder.append( path.value() );
-            this.url = urlBuilder.toString();
+            theUrl = urlBuilder.toString();
         }
+
+        // Removes PathParam regex if any. ie: {id: [0-9]{2,4}} becomes {id}
+        this.url = removeEnclosedCurlyBraces( theUrl ).replaceAll( "\\{([A-Za-z0-9-_]+)(\\s*:\\s*([^{}][^{}]*))*\\}", "{$1}" );
 
         VariableElement bodyParamVariable = null;
         for ( VariableElement variable : method.getParameters() ) {
@@ -71,6 +79,36 @@ public class RestServiceMethod {
         }
         this.bodyParamVariable = bodyParamVariable;
 
+    }
+
+    // Enclosed curly braces cannot be matched with a regex. Thus we remove them before applying the replaceAll method
+    private String removeEnclosedCurlyBraces( String str ) {
+        final char curlyReplacement = 6;
+
+        char[] chars = str.toCharArray();
+        int open = 0;
+        for ( int i = 0; i < chars.length; i++ ) {
+            if ( chars[i] == '{' ) {
+                if ( open != 0 ) chars[i] = curlyReplacement;
+                open++;
+            }
+            else if ( chars[i] == '}' ) {
+                open--;
+                if ( open != 0 ) {
+                    chars[i] = curlyReplacement;
+                }
+            }
+        }
+
+        char[] res = new char[chars.length];
+        int j = 0;
+        for( int i = 0; i < chars.length; i++ ) {
+            if( chars[i] != curlyReplacement ) {
+                res[j++] = chars[i];
+            }
+        }
+
+        return new String( Arrays.copyOf( res, j ) );
     }
 
     public ExecutableElement getMethod() {
